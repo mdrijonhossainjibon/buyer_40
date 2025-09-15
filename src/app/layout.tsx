cdb4aeae4e5073825bea6a886669e685adddf68b
mdrijonhossainjibon <arntoken.tem@gmail.com>
2025-09-16 00:21:42 +0600

    v-1.27

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import ReduxProvider from '@/components/ReduxProvider'
+import Script from 'next/script'
  
 const nunito = Nunito({
   subsets: ['latin'],
@@ -24,8 +25,8 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-        <script src="https://telegram.org/js/telegram-web-app.js" async></script>
-        <script src='//libtl.com/sdk.js' data-zone='9486612' data-sdk='show_9486612'></script>
+        <Script src="https://telegram.org/js/telegram-web-app.js" async />
+        <Script src='//libtl.com/sdk.js' data-zone='9486612' data-sdk='show_9486612' /> 
       </head>
       <body className={nunito.className}>
         <ReduxProvider>
