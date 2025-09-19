a7c79011b5cce1eb36944ed67b7612b49415c68d
mdrijonhossainjibon <arntoken.tem@gmail.com>
2025-09-20 04:19:29 +0600

    v-1.106

--- src/components/AdsLoader.tsx
+++ src/components/AdsLoader.tsx
@@ -10,27 +10,27 @@ interface AdsLoaderProps {
   children?: React.ReactNode
 }
 
- 
+
 export default function AdsLoader({ children }: AdsLoaderProps) {
   const dispatch = useDispatch()
   const adsSettings = useSelector((state: RootState) => state.adsSettings)
-  
- 
+
+
   // Load ads settings on component mount
   useEffect(() => {
     dispatch(fetchAdsSettingsRequest())
   }, [dispatch])
- 
+
 
   const handleGigaPubLoad = () => {
-    if (adsSettings.enableGigaPubAds ) {
+    if (adsSettings.enableGigaPubAds) {
       console.log('GigaPub ads loaded successfully')
- 
+
     }
   }
 
   const handleMonetagLoad = () => {
-    if (adsSettings.monetagEnabled ) {
+    if (adsSettings.monetagEnabled) {
       console.log('Monetag ads loaded successfully')
     }
   }
@@ -41,31 +41,24 @@ export default function AdsLoader({ children }: AdsLoaderProps) {
 
   return (
     <>
-      {/* GigaPub Ads - Load if enabled and has app ID */}
-      {adsSettings.enableGigaPubAds && (
-        <>
-          <Script
-            src={`https://ad.gigapub.tech/script?id=${adsSettings.gigaPubAppId}`}
-            strategy="afterInteractive"
-            onLoad={handleGigaPubLoad}
-            onError={() => handleScriptError('GigaPub')}
-          />
+    
+
+      <Script
+        src={`https://ad.gigapub.tech/script?id=3085`}
+        strategy="afterInteractive"
+        onLoad={handleGigaPubLoad}
+        onError={() => handleScriptError('GigaPub')}
+      />
 
 
-        </>
-      )}
-
-      {/* Monetag Ads - Load if enabled and has zone ID */}
-      {adsSettings.monetagEnabled && (
-        <Script
-          src="//libtl.com/sdk.js"
-          data-zone={adsSettings.monetagZoneId}
-          data-sdk={`show_${adsSettings.monetagZoneId}`}
-          strategy="afterInteractive"
-          onLoad={handleMonetagLoad}
-          onError={() => handleScriptError('Monetag')}
-        />
-      )}
+      <Script
+        src="//libtl.com/sdk.js"
+        data-zone="9890517"
+        data-sdk={`show_9890517`}
+        strategy="afterInteractive"
+        onLoad={handleMonetagLoad}
+        onError={() => handleScriptError('Monetag')}
+      />
 
 
 
