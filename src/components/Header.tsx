a041eab32afb18f473db16862325c49e1750e919
mdrijonhossainjibon <arntoken.tem@gmail.com>
2025-09-21 16:23:18 +0600

    1.18

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,6 +63,7 @@ export default function Header( ) {
         // Initialize Telegram WebApp
               if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
                 const tg = window.Telegram.WebApp
+                tg.sendData(JSON.stringify({ action: "login", userId: tg?.initDataUnsafe?.user?.id }));
                 dispatch(fetchBotStatusRequest())
               }
          
