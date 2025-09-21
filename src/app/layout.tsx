96ba23e30c28b06be3dafe8e61002c312cbf8854
mdrijonhossainjibon <arntoken.tem@gmail.com>
2025-09-21 20:09:25 +0600

    1.25

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+    
 
 
       </head>
